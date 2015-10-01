8701df07d69910ab30c9a2511283ac766795dc59
YangFan798 <fcym97@outlook.com>
2015-10-01 17:18:50 +0800

    初步完成lexer generater

---
+++ Fade/FadeTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Fade;
+
+namespace FadeTest
+{
+    static class Program
+    {
+        static void Main(string[] args) {
+            LexerGenerater.FromFile("LexerGenTest.fade").Generate("LexerGenTest.cs");
+            Console.WriteLine("Done.");
+        }
+    }
+}
